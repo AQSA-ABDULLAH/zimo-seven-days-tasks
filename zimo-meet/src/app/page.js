f28c21d0926a52358c1c0e41c68607b5bd91af68
AQSA-ABDULLAH <aqsaabdullah38403@gmail.com>
2024-11-13 09:20:21 +0500

    create section 2

--- zimo-meet/src/app/page.js
+++ zimo-meet/src/app/page.js
@@ -1,11 +1,13 @@
 import Header from "./components/Header";
 import Section1 from "./components/Section1";
+import Section2 from "./components/Section2";
 
 export default function Home() {
   return (
     <div>
       <Header />
       <Section1 />
+      <Section2 />
     </div>
   );
 }
