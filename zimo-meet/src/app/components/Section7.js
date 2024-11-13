dd1d39ff6ec5d385a21e889ace605bee9b9216e1
AQSA-ABDULLAH <aqsaabdullah38403@gmail.com>
2024-11-13 10:48:57 +0500

    create section 7

---
+++ zimo-meet/src/app/components/Section7.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+export default function Section7() {
+    return (
+        <section>
+            <video
+                src="assets/ZGS SCREEN VIDEO 2.mp4"
+                controls
+                autoPlay
+                loop
+                muted
+                playsInline
+                className="w-full h-screen object-cover"
+            >
+                Your browser does not support the video tag.
+            </video>
+        </section>
+    )
+}
