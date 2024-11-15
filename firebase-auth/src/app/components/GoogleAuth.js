92bb70764ad1f620c80ae0d2091598b99e59f562
AQSA-ABDULLAH <aqsaabdullah38403@gmail.com>
2024-11-15 09:59:08 +0500

    create google auth

--- firebase-auth/src/app/components/GoogleAuth.js
+++ firebase-auth/src/app/components/GoogleAuth.js
@@ -1,21 +1,21 @@
-import { getAuth } from "firebase-auth";
-import { useRouter } from "next/navigation";
-import { signInWithPopup, GoogleAuthProvider } from "firebase/auth";
-import { useEffect, useState } from "react";
+// Mark this file as a client component
+"use client";
 
-const Home = () => {
+import { getAuth, signInWithPopup, GoogleAuthProvider } from "firebase/auth";
+import { useRouter } from "next/navigation";
+import { useEffect, useState } from "react";
+import { app } from "../firebase/config";
+import Dashboard from "../dashboard/page";
+
+const GoogleAuth = () => {
     const [user, setUser] = useState(null);
     const router = useRouter();
 
     useEffect(() => {
         const auth = getAuth(app);
         const unsubscribe = auth.onAuthStateChanged((user) => {
-            if (user) {
-                setUser(user);
-            } else {
-                setUser(null);
-            }
-        })
+            setUser(user ? user : null);
+        });
 
         return () => unsubscribe();
     }, []);
@@ -27,23 +27,24 @@ const Home = () => {
             await signInWithPopup(auth, provider);
             router.push("/dashboard");
         } catch (error) {
-            console.error("Error signin with google:", error.message);
+            console.error("Error signing in with Google:", error.message);
         }
     };
+
     return (
         <div className="flex flex-col items-center justify-center h-screen">
             {user ? (
                 <Dashboard />
             ) : (
                 <button
-                    onclick={signInWithGoogle}
+                    onClick={signInWithGoogle}
                     className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
                 >
                     Sign In with Google
                 </button>
             )}
         </div>
-    )
+    );
 };
 
-export default Home;
+export default GoogleAuth;
